90b2df61f5587f3b6ab3282f6d9a146b9e601ab2
vscilab <a@b.com>
2020-01-16 16:44:02 -0800

    Hello

--- src/main.cc
+++ src/main.cc
@@ -1,4 +1,17 @@
-//
-// Created by aknight on 8/2/19.
-//
+/**
+ * CS V30 Beginning C++
+ * Assignment: EX01-HelloWorld
+ *
+ * Statement of code ownership: I hereby state that I have written all of this
+ * code and I have not copied this code from any other person or source.
+ *
+ * @author [CHANGE THIS INFORMATION]
+ */
+
+#include <iostream>
+
+int main(int argc, char *argv[]) {
+    std::cout << "Hello, World!" << std::endl;
+    return EXIT_SUCCESS;
+}
 
